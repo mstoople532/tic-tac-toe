9ac7b64ac0d706bbc768b9c4c2f8d0f9004986ff
Molly <mstoople@gmail.com>
2017-05-16 19:17:39 -0400

    Created a new app.rb file. Began refactoring game.rb into a class with methods.

--- game.rb
+++ game.rb
@@ -1,48 +1,58 @@
 require_relative "board"
 require_relative "player"
 
-puts "Player Two, X or O?"
+class Game
+  attr_accessor :player_one, :player_two, :board
+  def initialize
+    @player_one
+    @player_two
+    @board = Board.new
+  end
 
-while player_symbol = gets.chomp.upcase
-  if player_symbol == "X"
-    player_two = Player.new(player_symbol, "Player two")
-    player_one = Player.new("O", "Player one")
-    break
-  elsif player_symbol == "O"
-    player_two = Player.new(player_symbol, "Player two")
-    player_one = Player.new("X", "Player one")
-    break
-  else
+  def start!
     puts "Player Two, X or O?"
+    while player_symbol = gets.chomp.upcase
+      if player_symbol == "X"
+        player_two = Player.new(player_symbol, "Player two")
+        player_one = Player.new("O", "Player one")
+        break
+      elsif player_symbol == "O"
+        player_two = Player.new(player_symbol, "Player two")
+        player_one = Player.new("X", "Player one")
+        break
+      else
+        puts "Player Two, X or O?"
+      end
+    end
+
+    board = Board.new
+    move_count = 0
+
+    until board.available_moves.empty?
+      board.display
+
+      current_player = if move_count.even?
+                         player_one
+                       else
+                         player_two
+                       end
+
+      puts "#{current_player.player_type}, please choose a move"
+      player_move = gets.chomp.downcase
+      if board.move_unavailable?(player_move)
+        puts "Move already taken, try again!"
+        redo
+      end
+      current_player.player_move(board, player_move)
+      if current_player.player_win?
+        puts "#{current_player.player_type} wins!"
+        break
+      elsif board.available_moves.empty?
+        puts "Tie game!"
+        board.display
+        break
+      end
+      move_count += 1
+    end
   end
-end
-
-board = Board.new
-move_count = 0
-
-until board.available_moves.empty?
-  board.display
-
-  current_player = if move_count.even?
-                     player_one
-                   else
-                     player_two
-                   end
-
-  puts "#{current_player.player_type}, please choose a move"
-  player_move = gets.chomp.downcase
-  if board.move_unavailable?(player_move)
-    puts "Move already taken, try again!"
-    redo
-  end
-  current_player.player_move(board, player_move)
-  if current_player.player_win?
-    puts "#{current_player.player_type} wins!"
-    break
-  elsif board.available_moves.empty?
-    puts "Tie game!"
-    board.display
-    break
-  end
-  move_count += 1
 end
